31a7a5c937a0bae41f6a969611c8c056ef6eb695
Varun Kaushik <kaushikvarun3010@gmail.com>
2026-02-18 13:00:22 +0530

    Initial backend setup with MongoDB connection

---
+++ server/config/db.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+
+const connectDB = async () => {
+    console.log("MONGO_URI:", process.env.MONGO_URI);
+
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("MongoDB Connected Successfully");
+  } catch (error) {
+    console.error("MongoDB Connection Failed:");
+    console.error(error);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
---
+++ server/server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+
+dotenv.config();
+
+// CONNECT DATABASE FIRST
+connectDB();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("CodeArena API Running...");
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
