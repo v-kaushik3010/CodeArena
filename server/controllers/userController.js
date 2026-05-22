0bbaf428f5e2da5ae8f811a13761015c5a3ec1d2
v-kaushik3010 <kaushikvarun3010@gmail.com>
2026-05-22 17:32:40 +0530

    feat(leaderboard): add leaderboard controller

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -22,3 +22,26 @@ exports.getUserProfile = async (req, res) => {
     });
   }
 };
+// 🏆 Get Leaderboard
+exports.getLeaderboard = async (req, res) => {
+  try {
+
+    const users = await User.find()
+      .select("name score solvedProblems")
+      .sort({ score: -1 });
+
+    const leaderboard = users.map((user, index) => ({
+      rank: index + 1,
+      name: user.name,
+      score: user.score,
+      solvedCount: user.solvedProblems.length,
+    }));
+
+    res.status(200).json(leaderboard);
+
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
+};
